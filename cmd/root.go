a245849064aab48876a2fae612aa1fd07a57326e
Daryl Turner <daryl@layer-eight.uk>
2018-04-18 09:44:37 +0000

    bump version

--- cmd/root.go
+++ cmd/root.go
@@ -17,7 +17,7 @@ var rootCmd = &cobra.Command{
 	Long: `nlab can be used to create Linux bridges and taps to
 simulate complicated network topologies and launch KVM
 virtual-machines with sane defaults.`,
-	Version: "0.7.0",
+	Version: "0.8.0",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		if jsonOut == true {
 			log.SetFormatter(&log.JSONFormatter{})
